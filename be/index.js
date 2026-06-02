90a52bcaf178f78a07bae165835ab44a469bbd81
Andika <andikaraditya1@gmail.com>
2026-06-02 17:06:33 +0700

    chore: init express app

---
+++ be/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/test', (req, res) => {
+  res.json({ message: 'Hello, world!' })
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
+
+module.exports = app
